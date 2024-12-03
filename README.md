b3c5eadd2d9e4380aedfa56df9bcd2f28fb81c97
skovranek <59619403+skovranek@users.noreply.github.com>
2024-12-03 15:07:35 -0500

    skip 2.3: add badge to README.md

--- README.md
+++ README.md
@@ -12,6 +12,8 @@ Create a `.env` file in the root of the project with the following contents:
 PORT="8080"
 ```
 
+test/badge.svgtest
+
 Run the server:
 
 ```bash
--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,6 @@ export default {
   transform: {
     "^.+.tsx?$": ["ts-jest",{}],
   },
-  preset: "ts-jest",
-  verbose: true,
+  //preset: "ts-jest",
+  //verbose: true,
 };
